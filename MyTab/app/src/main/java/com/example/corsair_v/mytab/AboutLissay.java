409de013141aef1c62cc193d293fd1cc5fbe0015
Olivia1011 <2740879228@qq.com>
2017-12-19 21:26:23 +0800

    2017-12-19-zc

---
+++ MyTab/app/src/main/java/com/example/corsair_v/mytab/AboutLissay.java
@@ -0,0 +1,13 @@
+package com.example.corsair_v.mytab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutLissay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_lissay);
+    }
+}
